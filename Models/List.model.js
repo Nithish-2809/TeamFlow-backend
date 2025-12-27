24573f2ec344310cd6013c3e3a4c8d97c9eb44f5
Nithish Reddy <nithishreddy2809@gmail.com>
2025-12-27 19:06:16 +0530

    added list model

---
+++ Models/List.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose")
+
+const listSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    boardId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Board",
+      required: true
+    },
+
+    position: {
+      type: Number,
+      required: true
+    }
+  },
+  { timestamps: true }
+)
+
+const List = mongoose.model("List", listSchema)
+
+module.exports = List
