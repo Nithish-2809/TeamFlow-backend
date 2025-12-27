8fdbf53d7b5c488e6465285707fc4ba03b875d81
Nithish Reddy <nithishreddy2809@gmail.com>
2025-12-27 18:35:12 +0530

    added BoardMembership model

---
+++ Models/BoardMembership.model.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose")
+
+const BoardMembershipSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    boardId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Board",
+      required: true
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false
+    },
+    status: {
+      type: String,
+      enum: ["PENDING", "APPROVED"],
+      default: "PENDING"
+    }
+  },
+  { timestamps: true }
+)
+
+BoardMembershipSchema.index(
+  { userId: 1, boardId: 1 },
+  { unique: true }
+)
+
+const BoardMembership =  new mongoose.model("BoardMembership", BoardMembershipSchema)
+
+module.exports  = BoardMembership
